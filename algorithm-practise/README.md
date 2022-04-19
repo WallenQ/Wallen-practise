fe22424c1198b21e7f79912a2be9b3ec1c03f3dc
WallenQ <872465234@qq.com>
2022-04-19 15:02:26 +0800

    algorithm

---
+++ algorithm-practise/README.md
@@ -0,0 +1,6 @@
+#算法练习
+主要为Leetcode习题，与LeetCode对应关系
+1.twoSum
+2.AddTwoNumbers
+3.LengthOfLongestSubstring
+7.ReverseInteger
---
+++ algorithm-practise/src/com/wallen/practise/basesort/IArraySort.java
@@ -0,0 +1,19 @@
+package com.wallen.practise.basesort;
+
+/**
+ * 排序接口
+ *
+ * @author Wallen
+ * @date 2021年4月12日
+ */
+public interface IArraySort {
+    /**
+     * 排序
+     * https://gper.club/articles/7e7e7f7ff7g58gcbg67
+     *
+     * @param sourceArray
+     * @return
+     * @throws Exception
+     */
+    int[] sort(int[] sourceArray) throws Exception;
+}

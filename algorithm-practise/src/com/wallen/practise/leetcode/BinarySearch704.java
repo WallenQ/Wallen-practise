3334fefe7ce21155dba48340dcd0798ff2db8ca5
WallenQ <872465234@qq.com>
2022-08-19 11:24:59 +0800

    LeetCode704

--- algorithm-practise/src/com/wallen/practise/leetcode/BinarySearch704.java
+++ algorithm-practise/src/com/wallen/practise/leetcode/BinarySearch704.java
@@ -37,23 +37,48 @@ public class BinarySearch704 {
     }
 
     /**
-     * Better code
+     * 左闭右开区间
      *
      * @param nums
      * @param target
      * @return
      */
     public int searchBetter(int[] nums, int target) {
-        int startIndex = 0;
-        int endIndex = nums.length - 1;
-        while (startIndex <= endIndex) {
-            int midIndex = (startIndex + endIndex) / 2;
-            if (nums[midIndex] == target) {
-                return midIndex;
-            } else if (nums[midIndex] > target) {
-                endIndex = midIndex - 1;
-            } else if (nums[midIndex] < target) {
-                startIndex = midIndex + 1;
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 左闭右闭区间
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchBetter1(int[] nums, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             }
         }
         return -1;

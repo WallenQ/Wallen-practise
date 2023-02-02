91aaa4c42f3580c3e2458e38e2a428cc7879c78e
WallenQ <872465234@qq.com>
2023-02-02 10:54:56 +0800

    Java8 实战测试练习项目

---
+++ java8-practise/src/main/java/com/wallen/java8/practise/FutureTest.java
@@ -0,0 +1,51 @@
+package com.wallen.java8.practise;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author Wallen
+ * @date 2023/2/2 10:38
+ */
+public class FutureTest {
+
+    public static void main(String[] args) {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("demo-pool-%d").build();
+
+        //Common Thread Pool
+        ExecutorService executor = new ThreadPoolExecutor(5, 200,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        Future<Double> future = executor.submit(new Callable<Double>() {
+            @Override
+            public Double call() throws Exception {
+                Thread.sleep(500);
+                System.out.println(111);
+                return 111D;
+            }
+        });
+        System.out.println(222);
+        try {
+            future.get(1, TimeUnit.SECONDS);
+            System.out.println(333);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ java8-practise/src/main/java/com/wallen/java8/practise/Main.java
@@ -0,0 +1,11 @@
+package com.wallen.java8.practise;
+
+/**
+ * @author Wallen
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

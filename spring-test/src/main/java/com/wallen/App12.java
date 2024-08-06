ed52a9f62c821a66916c3479984bdb137990d1f6
Wallen <32693623+WallenQ@users.noreply.github.com>
2024-08-06 09:31:16 +0800

    2022新版SpringBoot2全套-视频151

--- spring-test/src/main/java/com/wallen/App12.java
+++ spring-test/src/main/java/com/wallen/App12.java
@@ -4,12 +4,14 @@ import com.wallen.bean.CartoonCatAndMouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author wallen
  * @date 2024/8/4 11:16
  */
 @SpringBootApplication
+@Import(CartoonCatAndMouse.class)
 public class App12 {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(App12.class);
--- spring-test/src/main/java/com/wallen/bean/CartoonCatAndMouse.java
+++ spring-test/src/main/java/com/wallen/bean/CartoonCatAndMouse.java
@@ -1,17 +1,36 @@
 package com.wallen.bean;
 
+import com.alibaba.druid.util.StringUtils;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
  * @author wallen
  * @date 2024/8/5 09:15
  */
-@Component
+@Data
+@EnableConfigurationProperties(CartoonProperties.class)
 public class CartoonCatAndMouse {
     private Cat cat;
     private Mouse mouse;
+    private CartoonProperties cartoonProperties;
 
-    public CartoonCatAndMouse() {
+    public CartoonCatAndMouse(CartoonProperties cartoonProperties) {
+        this.cartoonProperties = cartoonProperties;
+        if (cartoonProperties.getCat()!= null){
+            cat = new Cat();
+            cat.setName(cartoonProperties.getCat().getName());
+            cat.setAge(cartoonProperties.getCat().getAge());
+        }
+        if (cartoonProperties.getMouse()!= null){
+            mouse = new Mouse();
+            mouse.setName(cartoonProperties.getMouse().getName());
+            mouse.setAge(cartoonProperties.getMouse().getAge());
+        }
+    }
+    /*public CartoonCatAndMouse() {
         Cat cat = new Cat();
         cat.setName("tom");
         cat.setAge(3);
@@ -20,7 +39,7 @@ public class CartoonCatAndMouse {
         mouse.setName("jerry");
         mouse.setAge(4);
         this.mouse = mouse;
-    }
+    }*/
 
     public void play() {
         System.out.println(cat.getAge() + "岁的" + cat.getName() + "和" + mouse.getAge() + "岁的" + mouse.getName() + "打起来了");

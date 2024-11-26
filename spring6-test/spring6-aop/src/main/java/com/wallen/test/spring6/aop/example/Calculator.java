08a1255afe2930bb3c26c235be34b593faebe070
Wallen <32693623+WallenQ@users.noreply.github.com>
2024-11-26 19:57:48 +0800

    尚硅谷2023最新版spring6:静态代理

---
+++ spring6-test/spring6-aop/src/main/java/com/wallen/test/spring6/aop/example/Calculator.java
@@ -0,0 +1,15 @@
+package com.wallen.test.spring6.aop.example;
+
+/**
+ * @author Wallen
+ * @date 2024/11/26 19:26
+ */
+public interface Calculator {
+    int add(int i, int j);
+
+    int sub(int i, int j);
+
+    int mul(int i, int j);
+
+    int div(int i, int j);
+}
---
+++ spring6-test/spring6-aop/src/main/java/com/wallen/test/spring6/aop/example/CalculatorStaticProxy.java
@@ -0,0 +1,46 @@
+package com.wallen.test.spring6.aop.example;
+
+/**
+ * @author Wallen
+ * @date 2024/11/26 19:44
+ */
+public class CalculatorStaticProxy implements Calculator {
+    //被代理目标对象传递过来
+    private Calculator calculator;
+
+    public CalculatorStaticProxy(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    @Override
+    public int add(int i, int j) {
+        System.out.println("[日志] add 方法开始了， 参数是：" + i + "," + j);
+        int result = calculator.add(i, j);
+        System.out.println("[日志] add 方法结束了， 结果是：" + result);
+        return result;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        System.out.println("[日志] sub 方法开始了， 参数是：" + i + "," + j);
+        int result = calculator.sub(i, j);
+        System.out.println("[日志] sub 方法结束了， 结果是：" + result);
+        return result;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        System.out.println("[日志] mul 方法开始了， 参数是：" + i + "," + j);
+        int result = calculator.mul(i, j);
+        System.out.println("[日志] mul 方法结束了， 结果是：" + result);
+        return result;
+    }
+
+    @Override
+    public int div(int i, int j) {
+        System.out.println("[日志] div 方法开始了， 参数是：" + i + "," + j);
+        int result = calculator.div(i, j);
+        System.out.println("[日志] div 方法结束了， 结果是：" + result);
+        return result;
+    }
+}

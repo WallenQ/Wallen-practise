dec7dae14c4a75c3679dbeac7eb808aef2075bd5
Wallen <32693623+WallenQ@users.noreply.github.com>
2025-03-27 16:25:02 +0800

    Component注解使用：Bean基本注解开发

--- spring-test/annotation/src/main/java/com/wallen/practise/spring/test/annotation/ApplicationContextTest.java
+++ spring-test/annotation/src/main/java/com/wallen/practise/spring/test/annotation/ApplicationContextTest.java
@@ -1,7 +1,6 @@
 package com.wallen.practise.spring.test.annotation;
 
 import com.wallen.practise.spring.test.annotation.service.UserService;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -10,8 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class ApplicationContextTest {
     public static void main(String[] args) {
-        ApplicationContext context     = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService        userService = context.getBean(UserService.class);
+        ClassPathXmlApplicationContext context     = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService                    userService = context.getBean(UserService.class);
         System.out.println(userService);
+        context.close();
     }
 }
--- spring-test/annotation/src/main/java/com/wallen/practise/spring/test/annotation/service/impl/UserServiceImpl.java
+++ spring-test/annotation/src/main/java/com/wallen/practise/spring/test/annotation/service/impl/UserServiceImpl.java
@@ -1,12 +1,32 @@
 package com.wallen.practise.spring.test.annotation.service.impl;
 
 import com.wallen.practise.spring.test.annotation.service.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
 
 /**
  * @author Wallen
  * @date 2025/3/26 17:58
  */
 @Component
+@Scope("singleton")
+@Lazy
 public class UserServiceImpl implements UserService {
+    public UserServiceImpl() {
+        System.out.println("UserServiceImpl create...");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("UserServiceImpl init...");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("UserServiceImpl destroy...");
+    }
 }

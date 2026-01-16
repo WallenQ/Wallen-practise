93e0ae7b92742c37ec220d28e2420e4c9313ac11
qianwenlong <872465234@qq.com>
2026-01-16 15:52:09 +0800

    LeetCode 707.设计链表-testing

--- algorithm-practise/src/com/wallen/practise/leetcode/linked/MyLinkedList707.java
+++ algorithm-practise/src/com/wallen/practise/leetcode/linked/MyLinkedList707.java
@@ -23,7 +23,7 @@ package com.wallen.practise.leetcode.linked;
  * <p>
  * 输入
  * ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
- * [[], [1], [3], [1, 2], [1], [1], [1]]
+ * [ [1], [3], [1, 2], [1], [1], [1]]
  * 输出
  * [null, null, null, null, 2, null, 3]
  * <p>
@@ -48,28 +48,149 @@ package com.wallen.practise.leetcode.linked;
  */
 public class MyLinkedList707 {
     public static void main(String[] args) {
-        MyLinkedList707 myLinkedList707 = new MyLinkedList707();
-        myLinkedList707.addAtHead(1);
-        myLinkedList707.addAtTail(3);
-        //myLinkedList707.addAtIndex(1,2);
+        //MyLinkedList707 myLinkedList707 = new MyLinkedList707();
+        //myLinkedList707.addAtHead(1);
+        //myLinkedList707.addAtTail(3);
+        //myLinkedList707.addAtIndex(1, 2);
         //System.out.println(myLinkedList707.get(1));
-        myLinkedList707.deleteAtIndex(0);
-        System.out.println(myLinkedList707.get(1));
+        //myLinkedList707.deleteAtIndex(0);
+        //System.out.println(myLinkedList707.get(1));
+
+        //["MyLinkedList","addAtHead","addAtHead","addAtHead","addAtIndex","deleteAtIndex","addAtHead","addAtTail","get","addAtHead","addAtIndex","addAtHead"]
+        //[[7],[2],[1],[3,0],[2],[6],[4],[4],[4],[5,0],[6]]
+        //MyLinkedList707 myLinkedList707 = new MyLinkedList707();
+        //myLinkedList707.addAtHead(7);
+        //myLinkedList707.addAtHead(2);
+        //myLinkedList707.addAtHead(1);
+        //myLinkedList707.addAtIndex(3, 0);
+        //myLinkedList707.deleteAtIndex(2);
+        //myLinkedList707.addAtHead(6);
+        //myLinkedList707.addAtTail(4);
+        //myLinkedList707.get(4);
+        //myLinkedList707.addAtHead(4);
+        //myLinkedList707.addAtIndex(5, 0);
+        //myLinkedList707.addAtHead(6);
+
+        //["MyLinkedList","addAtHead","addAtTail","addAtIndex","get","deleteAtIndex","get"]
+        //[[1],[3],[1,2],[1],[1],[1]]
+        //MyLinkedList707 myLinkedList707 = new MyLinkedList707();
+        //myLinkedList707.addAtHead(1);
+        //myLinkedList707.addAtTail(3);
+        //myLinkedList707.addAtIndex(1,2);
+        //myLinkedList707.get(1);
+        //myLinkedList707.deleteAtIndex(1);
+        //myLinkedList707.get(1);
+
+        //["MyLinkedList","addAtHead","addAtIndex","addAtIndex","addAtHead","deleteAtIndex","addAtIndex","addAtHead","addAtTail","addAtHead","get","addAtTail","addAtTail","addAtIndex","addAtTail","addAtTail","addAtHead","addAtTail","addAtHead","addAtTail","addAtHead","addAtTail","addAtTail","addAtHead","addAtTail","addAtIndex","addAtHead","deleteAtIndex","addAtHead","addAtHead","addAtHead","addAtHead","addAtIndex","addAtHead","addAtTail","addAtHead","deleteAtIndex","addAtTail","deleteAtIndex","addAtTail","addAtTail","addAtTail","addAtTail","addAtHead","addAtTail","get","addAtIndex","get","deleteAtIndex","addAtTail","addAtHead","addAtTail","addAtTail","addAtIndex","addAtHead","addAtHead","addAtHead","addAtHead","addAtHead","addAtTail","deleteAtIndex","deleteAtIndex","addAtHead","addAtHead","addAtTail","addAtHead","get","addAtIndex","addAtIndex","get","addAtTail","get","addAtTail","deleteAtIndex","get","addAtTail","addAtTail","addAtHead","addAtTail","deleteAtIndex","addAtHead","addAtHead","addAtHead","deleteAtIndex","addAtTail","addAtIndex","addAtTail","addAtTail","addAtIndex","addAtIndex","addAtHead","addAtIndex","addAtHead","addAtHead","addAtTail","addAtIndex","addAtTail","get","addAtHead","addAtTail","addAtHead","addAtHead"]
+        //[[86],[1,54],[1,14],[83],[4],[3,18],[46],[3],[76],[5],[79],[74],[7,28],[68],[16],[82],[24],[30],[96],[21],[79],[66],[2],[2],[7,57],[59],[21],[19],[55],[46],[17],[16,41],[97],[85],[63],[30],[11],[16],[91],[29],[47],[20],[12],[80],[15],[12,8],[21],[30],[11],[54],[51],[41],[8,88],[62],[7],[59],[73],[73],[55],[9],[7],[49],[99],[17],[44],[11],[26,86],[10,99],[19],[71],[29],[32],[2],[3],[16],[2],[83],[31],[3],[23],[64],[96],[27],[81],[12,78],[21],[69],[5,35],[8,72],[60],[19,73],[55],[83],[86],[31,70],[49],[19],[64],[22],[25],[13]]
+
+        MyLinkedList707 myLinkedList707 = new MyLinkedList707();
+        myLinkedList707.addAtHead(86);
+        myLinkedList707.addAtIndex(1,54);
+        myLinkedList707.addAtIndex(1,14);
+        myLinkedList707.deleteAtIndex(4);
+        myLinkedList707.addAtIndex(3,18);
+        myLinkedList707.addAtTail(3);
+        myLinkedList707.addAtHead(76);
+        myLinkedList707.get(5);
+        myLinkedList707.addAtTail(79);
+        myLinkedList707.addAtTail(74);
+        myLinkedList707.addAtIndex(7,28);
+        myLinkedList707.addAtTail(68);
+        myLinkedList707.addAtHead(82);
+        myLinkedList707.addAtTail(24);
+        myLinkedList707.addAtHead(30);
+        myLinkedList707.addAtTail(96);
+        myLinkedList707.addAtHead(21);
+        myLinkedList707.addAtTail(79);
+        myLinkedList707.addAtTail(66);
+        myLinkedList707.addAtHead(2);
+        myLinkedList707.addAtTail(2);
+        myLinkedList707.addAtIndex(7,57);
+        myLinkedList707.addAtHead(59);
+        myLinkedList707.deleteAtIndex(21);
+        myLinkedList707.addAtHead(19);
+        myLinkedList707.addAtHead(55);
+        myLinkedList707.addAtHead(46);
+        myLinkedList707.addAtHead(17);
+        myLinkedList707.addAtIndex(16,41);
+        myLinkedList707.addAtHead(97);
+        myLinkedList707.addAtTail(85);
+        myLinkedList707.addAtHead(63);
+        myLinkedList707.deleteAtIndex(30);
+        myLinkedList707.addAtTail(11);
+        myLinkedList707.deleteAtIndex(16);
+        myLinkedList707.addAtTail(91);
+        myLinkedList707.addAtTail(29);
+        myLinkedList707.addAtTail(47);
+        myLinkedList707.addAtTail(20);
+        myLinkedList707.addAtHead(12);
+        myLinkedList707.addAtTail(80);
+        myLinkedList707.get(15);
+        myLinkedList707.addAtIndex(12,8);
+        myLinkedList707.get(21);
+        myLinkedList707.deleteAtIndex(30);
+        myLinkedList707.addAtTail(11);
+        myLinkedList707.addAtHead(54);
+        myLinkedList707.addAtTail(51);
+        myLinkedList707.addAtTail(41);
+        myLinkedList707.addAtIndex(8,88);
+        myLinkedList707.addAtHead(62);
+        myLinkedList707.addAtHead(7);
+        myLinkedList707.addAtHead(59);
+        myLinkedList707.addAtHead(73);
+        myLinkedList707.addAtHead(73);
+        myLinkedList707.addAtTail(55);
+        myLinkedList707.deleteAtIndex(9);
+        myLinkedList707.deleteAtIndex(7);
+        myLinkedList707.addAtHead(49);
+        myLinkedList707.addAtHead(99);
+        myLinkedList707.addAtTail(17);
+        myLinkedList707.addAtHead(44);
+        myLinkedList707.get(11);
+        myLinkedList707.addAtIndex(26,86);
+        myLinkedList707.addAtIndex(10,99);
+        myLinkedList707.get(19);
+        myLinkedList707.addAtTail(71);
+        myLinkedList707.get(29);
+        myLinkedList707.addAtTail(32);
+        myLinkedList707.deleteAtIndex(2);
+        myLinkedList707.get(3);
+        myLinkedList707.addAtTail(16);
+        myLinkedList707.addAtTail(2);
+        myLinkedList707.addAtHead(83);
+        myLinkedList707.addAtTail(31);
+        myLinkedList707.deleteAtIndex(3);
+        myLinkedList707.addAtHead(23);
+        myLinkedList707.addAtHead(64);
+        myLinkedList707.addAtHead(96);
+        myLinkedList707.deleteAtIndex(27);
+        myLinkedList707.addAtTail(81);
+        myLinkedList707.addAtIndex(12,78);
+        myLinkedList707.addAtTail(21);
+        myLinkedList707.addAtTail(69);
+        myLinkedList707.addAtIndex(5,35);
+        myLinkedList707.addAtIndex(8,72);
+        myLinkedList707.addAtHead(60);
+        myLinkedList707.addAtIndex(19,73);
+        myLinkedList707.addAtHead(55);
+        myLinkedList707.addAtHead(83);
+        myLinkedList707.addAtTail(86);
+        myLinkedList707.addAtIndex(31,70);
+        myLinkedList707.addAtTail(49);
+        myLinkedList707.get(19);
+        myLinkedList707.addAtHead(64);
+        myLinkedList707.addAtTail(22);
+        myLinkedList707.addAtHead(25);
+        myLinkedList707.addAtHead(13);
     }
-    /**
-     * Your MyLinkedList object will be instantiated and called as such:
-     * MyLinkedList obj = new MyLinkedList();
-     * int param_1 = obj.get(index);
-     * obj.addAtHead(val);
-     * obj.addAtTail(val);
-     * obj.addAtIndex(index,val);
-     * obj.deleteAtIndex(index);
-     */
-    public class LinkedNode {
+
+    public static class LinkedNode {
         int val;
         LinkedNode next;
+
         LinkedNode(int val) {
-            this.val=val;
+            this.val = val;
         }
     }
 
@@ -77,77 +198,79 @@ public class MyLinkedList707 {
     LinkedNode node;
 
     public MyLinkedList707() {
-        this.node = new LinkedNode(0);
+        this.node = null;
         this.size = 0;
     }
 
     public int get(int index) {
-        if (index < 0 || index >= this.size) {
+        if (index < 0 || index >= size) {
             return -1;
         }
-        LinkedNode current = this.node;
-        for (int i = 0; i <= index; i++) {
+        LinkedNode current = node;
+        int i = 0;
+        while (i < index) {
             current = current.next;
+            i++;
         }
-        return current.val;
+        return current == null ? -1 : current.val;
     }
 
     public void addAtHead(int val) {
-        LinkedNode newNode = new LinkedNode(val);
-        if (this.size == 0) {
-            this.node = newNode;
-        } else {
-            newNode.next = this.node;
-            this.node = newNode;
-        }
-        size++;
+        addAtIndex(0, val);
     }
 
     public void addAtTail(int val) {
-        LinkedNode newNode = new LinkedNode(val);
-
-        if (this.size == 0) {
-            this.node = newNode;
-        } else {
-            LinkedNode last = this.node;
-            for (int i = 0; i <= this.size; i++) {
-                last = last.next;
-            }
-
-            last.next = newNode;
-        }
-        size++;
+        addAtIndex(size, val);
     }
 
     public void addAtIndex(int index, int val) {
-        if (index < 0 || index > this.size){
+        if (index < 0 || index > size) {
             return;
         }
 
-        LinkedNode newNode = new LinkedNode(val);
-        if (this.size == 0) {
-            this.node = newNode;
+        LinkedNode linkedNode = new LinkedNode(val);
+        if (size == 0) {
+            this.node = linkedNode;
         } else {
-            LinkedNode last = this.node;
-            for (int i = 0; i <= index; i++) {
-                last = last.next;
+            LinkedNode pre = null;
+            LinkedNode current = node;
+            if (index == 0){
+                this.node = linkedNode;
+            } else {
+                int i = 0;
+                while (i < index) {
+                    pre = current;
+                    current = current.next;
+                    i++;
+                }
             }
-
-            newNode.next = last.next;
-            last.next = newNode;
+            if (pre != null) {
+                pre.next = linkedNode;
+            }
+            linkedNode.next = current;
         }
+
         size++;
     }
 
     public void deleteAtIndex(int index) {
-        if (index < 0 || index >= this.size) {
+        if (index < 0 || index >= size) {
             return;
         }
-        LinkedNode current = this.node;
-        for (int i = 0; i <= index; i++) {
+        LinkedNode pre = null;
+        LinkedNode current = node;
+
+        int i = 0;
+        while (i < index) {
+            pre = current;
             current = current.next;
+            i++;
         }
-        //current.next = current.next.next;
+        if (pre == null) {
+            this.node = current.next;
+        } else {
+            pre.next = current.next;
+        }
         size--;
     }
 }
